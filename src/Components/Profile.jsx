87f52c7ffbb5d5256b70f6c9a435f44501352063
EdwardDNB <edvarddnbs@gmail.com>
2022-07-13 13:59:37 +0300

    Git

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,23 @@
+const Profile = () => {
+	return <div className='content'>
+		<div>
+			<img src='https://previews.123rf.com/images/stegarau/stegarau1605/stegarau160500011/58593212-big-cruise-ship-and-plane-over-the-sea-as-theme-for-vacations-and-traveling.jpg' />
+		</div>
+		<div>
+			ava + deskription
+		</div>
+		<div>
+			My post
+			<div>
+				New post
+			</div>
+			<div>
+				Post 1
+			</div>
+			<div>
+				Post 2
+			</div>
+		</div>
+	</div>
+}
+export default Profile
