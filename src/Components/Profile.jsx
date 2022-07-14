011d0d3e5104dcd5a689590e92f37b3963a454b1
EdwardDNB <edvarddnbs@gmail.com>
2022-07-14 17:28:57 +0300

    14 Lesson

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,5 +1,6 @@
+import s from './Profile.module.css'
 const Profile = () => {
-	return <div className='content'>
+	return <div className={s.content}>
 		<div>
 			<img src='https://previews.123rf.com/images/stegarau/stegarau1605/stegarau160500011/58593212-big-cruise-ship-and-plane-over-the-sea-as-theme-for-vacations-and-traveling.jpg' />
 		</div>
@@ -11,11 +12,13 @@ const Profile = () => {
 			<div>
 				New post
 			</div>
-			<div>
-				Post 1
-			</div>
-			<div>
-				Post 2
+			<div className={s.posts}>
+				<div className={s.item}>
+					Post 1
+				</div>
+				<div className={s.item}>
+					Post 2
+				</div>
 			</div>
 		</div>
 	</div>
