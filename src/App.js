a2ab72e4b00b2076e21c690d99e936f3b299c435
EdwardDNB <edvarddnbs@gmail.com>
2023-02-25 13:10:48 +0200

    Lesson 94 Lazy Suspense

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {Route, Routes} from 'react-router-dom';
 import News from './Components/News/News';
 import Music from './Components/Music/Music';
 import Settings from './Components/Settings/Settings';
-import DialogsContainer from './Components/Dialogs/DialogsContainer';
+//import DialogsContainer from './Components/Dialogs/DialogsContainer';
 import UsersContainer from './Components/Users/UsersContainer';
 import ProfileContainer from './Components/Profile/ProfileContainer';
 import HeaderContainer from './Components/Header/HeaderContainer';
@@ -13,6 +13,8 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {setInitial} from './Redax/app-reducer';
 import Preloader from './Components/Preloader/Preloader';
+
+const DialogsContainer = React.lazy(() => import('./Components/Dialogs/DialogsContainer'));
 
 class App extends React.Component {
   componentDidMount() {
@@ -26,16 +28,18 @@ class App extends React.Component {
         <HeaderContainer />
         <Navbar />
         <div className="app-wrapper-content">
-          <Routes>
-            <Route path="profile/" element={<ProfileContainer />} />
-            <Route path="/Users/profile/:userId" element={<ProfileContainer />} />
-            <Route path="Dialogs" element={<DialogsContainer />} />
-            <Route path="News" element={<News />} />
-            <Route path="Music" element={<Music />} />
-            <Route path="Settings" element={<Settings />} />
-            <Route path="Users" element={<UsersContainer />} />
-            <Route path="Login" element={<Login />} />
-          </Routes>
+          <React.Suspense fallback={<Preloader />}>
+            <Routes>
+              <Route path="profile/" element={<ProfileContainer />} />
+              <Route path="/Users/profile/:userId" element={<ProfileContainer />} />
+              <Route path="Dialogs" element={<DialogsContainer />} />
+              <Route path="News" element={<News />} />
+              <Route path="Music" element={<Music />} />
+              <Route path="Settings" element={<Settings />} />
+              <Route path="Users" element={<UsersContainer />} />
+              <Route path="Login" element={<Login />} />
+            </Routes>
+          </React.Suspense>
         </div>
       </div>
     );
