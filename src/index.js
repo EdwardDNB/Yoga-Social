85aa03e23b99155e430b2eac6808028a2268bece
EdwardDNB <edvarddnbs@gmail.com>
2023-03-30 16:59:17 +0300

    Lesson 95 GH-PAGES

--- src/index.js
+++ src/index.js
@@ -2,24 +2,19 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter} from 'react-router-dom';
 
-import {Provider} from "react-redux";
-import store from "./Redax/redaxStore";
-
+import {Provider} from 'react-redux';
+import store from './Redax/redaxStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
-    root.render(
-        <React.StrictMode>
-            <BrowserRouter>
-               <Provider store={store}>
-                   <App/>
-               </Provider>
-            </BrowserRouter>
-        </React.StrictMode>
-    )
-
-
-
+root.render(
+  <React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
